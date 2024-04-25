555cce47ab7a4e35793083087bcb5f1bfaace8b2
anand jain <anandj@uchicago.edu>
2024-04-25 11:15:17 -0700

    taking a pic from a webcam in rust ffmpeg and using `image` to write

--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,136 @@
-extern crate ffmpeg_sys_the_third;
-extern crate image;
+extern crate ffmpeg_the_third as ffmpeg;
 
-use ffmpeg_sys_the_third::av_get_pix_fmt_name;
-use ffmpeg_sys_the_third::AVMediaType::AVMEDIA_TYPE_VIDEO;
-use ffmpeg_sys_the_third::AVPixelFormat;
-use ffmpeg_sys_the_third::*;
-use ffmpeg_the_third as ffmpeg;
-use image::{ImageBuffer, Rgb};
-use std::ffi::CStr;
+use crate::ffmpeg::format::{input, Pixel};
+use crate::ffmpeg::media::Type;
+use crate::ffmpeg::software::scaling::{context::Context, flag::Flags};
+use crate::ffmpeg::util::frame::video::Video;
+use anyhow;
 use std::ffi::CString;
-use std::mem;
-use std::ptr;
-use std::slice;
+use std::fs::File;
+use std::io::prelude::*;
+use std::{env, ptr};
+fn main() -> Result<(), anyhow::Error> {
+    ffmpeg::init().unwrap();
+    // input()
 
-fn main() {
-    unsafe {
-        // Register all available file formats and codecs
-        avdevice_register_all();
+    let device_name =
+        CString::new("video=Microsoft® LifeCam Studio(TM)").expect("CString::new failed");
+    let format_name = CString::new("dshow").expect("CString::new failed");
+    // av_find_input_format("d")
+    unsafe { 
 
-        let mut format_ctx = ptr::null_mut();
-
-        let device_name =
-            CString::new("video=Microsoft® LifeCam Studio(TM)").expect("CString::new failed");
-            
-
-        let device_name =
-            CString::new("video=Integrated Webcam").expect("CString::new failed");
-
-        let format_name = CString::new("dshow").expect("CString::new failed");
-        let fmt = av_find_input_format(format_name.as_ptr());
-
-        let mut options = ptr::null_mut();
-        // av_dict_set(
-        //     &mut options,
-        //     CString::new("video_size").unwrap().as_ptr(),
-        //     CString::new("1920x1080").unwrap().as_ptr(),
-        //     0,
-        // );
-
-        if avformat_open_input(&mut format_ctx, device_name.as_ptr(), fmt, &mut options) != 0 {
-            eprintln!("Cannot open input device with specified options");
-            return;
-        }
-
-        if avformat_find_stream_info(format_ctx, ptr::null_mut()) < 0 {
-            eprintln!("Couldn't find stream information");
-            avformat_close_input(&mut format_ctx);
-            return;
-        }
-
-        let mut video_stream_index = -1;
-        for i in 0..(*format_ctx).nb_streams {
-            let stream = *(*format_ctx).streams.offset(i as isize);
-            if (*(*stream).codecpar).codec_type == AVMEDIA_TYPE_VIDEO {
-                video_stream_index = i as i32;
-                let codecpar = *(*stream).codecpar;
-                println!("Resolution: {}x{}", codecpar.width, codecpar.height);
-                break;
-            }
-        }
-
-        if video_stream_index == -1 {
-            eprintln!("No video stream found");
-            avformat_close_input(&mut format_ctx);
-            return;
-        }
-
-        let codec_id =
-            (*(*(*(*format_ctx).streams.offset(video_stream_index as isize))).codecpar).codec_id;
-        let codec = avcodec_find_decoder(codec_id);
-        if codec.is_null() {
-            eprintln!("Codec not found");
-            avformat_close_input(&mut format_ctx);
-            return;
-        }
-
-        let mut codec_ctx = avcodec_alloc_context3(codec);
-        avcodec_parameters_to_context(
-            codec_ctx,
-            (*(*(*format_ctx).streams.offset(video_stream_index as isize))).codecpar,
-        );
-        avcodec_open2(codec_ctx, codec, &mut options);
-
-        let mut frame = av_frame_alloc();
-
-        let mut packet: AVPacket = mem::zeroed();
-        av_init_packet(&mut packet);
-
-        while av_read_frame(format_ctx, &mut packet) >= 0 {
-            if packet.stream_index == video_stream_index {
-                if avcodec_send_packet(codec_ctx, &packet) == 0 {
-                    while avcodec_receive_frame(codec_ctx, frame) == 0 {
-                        let f = (*frame);
-                        println!("frame pkt_size: {:?}", f.pkt_size);
-                        // let foo = CStr::from_ptr((*frame).format as i8);
-                        // AVPixelFormat::
-                        println!("frame format: {:?}", f.format);
-                        let pix_fmt = std::mem::transmute::<i32, AVPixelFormat>(f.format);
-                        let format_name_ptr = CStr::from_ptr(av_get_pix_fmt_name(pix_fmt))
-                            .to_str()
-                            .unwrap();
-                        println!("pix fmt: {:?}", format_name_ptr);
-                        // let foo2 = av_get_pix_fmt_name(f.format);
-                        // (*frame).fo
-                        let fd = (*frame).data[0];
-                        let dim = ((*frame).width, (*frame).height);
-                        // println!("Frame received: {:?}", fd.);
-                        let pix_fmt_desc = av_pix_fmt_desc_get(pix_fmt);
-                        println!("pix_fmt_desc: {:?}", *pix_fmt_desc);
-
-
-                        let buffer_bytes_size = av_image_get_buffer_size(
-                            AVPixelFormat::AV_PIX_FMT_RGB24,
-                            dim.0,
-                            dim.1,
-                            0,
-                        ) as usize;
-                        println!("buffer_bytes_size: {:?}", buffer_bytes_size);
-                        let data = slice::from_raw_parts(
-                            fd,
-                            ((*frame).linesize[0] * (*frame).height) as usize,
-                        );
-                        
-                        // sws_convert
-                        println!("Frame received: {:?}", data.len());
-                        let buffer = ImageBuffer::<Rgb<u8>, _>::from_raw(
-                            dim.0 as u32,
-                            dim.1 as u32,
-                            data.to_vec(),
-                        )
-                        .unwrap();
-                        buffer.save("frame.png").unwrap();
-                        println!("Image saved as 'frame.png'");
-                        break;
-                    }
-                }
-                av_packet_unref(&mut packet);
-                break;
-            }
-            av_packet_unref(&mut packet);
-        }
-
-        av_frame_free(&mut frame);
-        avcodec_free_context(&mut codec_ctx);
-        avformat_close_input(&mut format_ctx);
     }
+    println!("b");
+    let mut ps = ptr::null_mut();
+    let mut options = ptr::null_mut();
+
+    let mut ictx = unsafe {
+        avdevice_register_all();
+        let fmt = av_find_input_format(format_name.as_ptr());
+        println!("fmt : {:?}", (*fmt).long_name);
+        if avformat_open_input(&mut ps, device_name.as_ptr(), fmt, options) != 0 {
+            eprintln!("Cannot open input device with specified options");
+            return Err(anyhow::anyhow!(
+                "Cannot open input device with specified options"
+            ));
+        }
+
+        Input::wrap(ps)
+    };
+    println!("Input : {:?}", 55);
+
+    let input = ictx
+        .streams()
+        .best(Type::Video)
+        .ok_or(ffmpeg::Error::StreamNotFound)?;
+    println!("Input : {:?}", input);
+    let video_stream_index = input.index();
+
+    let context_decoder = ffmpeg::codec::context::Context::from_parameters(input.parameters())?;
+    // unsafe {
+    //     println!("Context Decoder : {:?}", *context_decoder.as_ptr());
+    // }
+    let mut decoder = context_decoder.decoder().video()?;
+    // println!("Decoder : {:?}", decoder);
+    let mut scaler = Context::get(
+        decoder.format(),
+        decoder.width(),
+        decoder.height(),
+        Pixel::RGB24,
+        decoder.width(),
+        decoder.height(),
+        Flags::BILINEAR,
+    )?;
+    // println!("Scaler : {:?}", scaler);
+
+    let mut frame_index = 0;
+
+    let mut receive_and_process_decoded_frames =
+        |decoder: &mut ffmpeg::decoder::Video| -> Result<(), ffmpeg::Error> {
+            let mut decoded = Video::empty();
+            while decoder.receive_frame(&mut decoded).is_ok() {
+                let old_fmt = decoded.format();
+                println!("old planes: {:?}", decoded.planes());
+                println!("pix_fmt: {:?}", old_fmt);
+
+                let mut rgb_frame = Video::empty();
+                scaler.run(&decoded, &mut rgb_frame)?;
+                println!("planes: {:?}", rgb_frame.planes());
+                println!("converted pix_fmt: {:?}", rgb_frame.format());
+                save_file2(&rgb_frame, frame_index).unwrap();
+                frame_index += 1;
+            }
+            Ok(())
+        };
+
+    for (i, (stream, packet)) in ictx.packets().enumerate() {
+        if stream.index() == video_stream_index {
+            if i == 10 {
+                break;
+            }
+            decoder.send_packet(&packet)?;
+            receive_and_process_decoded_frames(&mut decoder)?;
+        }
+    }
+
+    decoder.send_eof()?;
+    receive_and_process_decoded_frames(&mut decoder)?;
+
+    Ok(())
+}
+
+use ffmpeg::format::context::Input;
+use ffmpeg_sys_the_third::{av_find_input_format, avdevice_register_all, avformat_open_input};
+use image::{ImageBuffer, RgbImage};
+use std::io::Result as IoResult;
+
+fn save_file2(frame: &Video, index: usize) -> IoResult<()> {
+    let width = frame.width() as u32;
+    let height = frame.height() as u32;
+    let frame_data = frame.data(0);
+
+    // Creating an ImageBuffer from raw frame data
+    let img: RgbImage =
+        ImageBuffer::from_raw(width, height, frame_data.to_vec()).ok_or(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Cannot create image buffer",
+        ))?;
+
+    // Save to file
+    img.save(format!("frame{index}.png")).unwrap();
+
+    Ok(())
+}
+
+fn save_file(frame: &Video, index: usize) -> std::result::Result<(), std::io::Error> {
+    // frame.data(index)
+    // frame.data.len()
+    println!("Frame received: {:?}", frame.data(0).len());
+    // let x = frame.;
+    // println!("Frame received: {:?}", frame.data(1).len());
+    println!("converted pix_fmt: {:?}", frame.format());
+    let mut file = File::create(format!("frame{index}.ppm"))?;
+    file.write_all(format!("P6\n{} {}\n255\n", frame.width(), frame.height()).as_bytes())?;
+    file.write_all(frame.data(0))?;
+    Ok(())
 }
