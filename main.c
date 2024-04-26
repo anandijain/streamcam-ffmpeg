b5092dee213cbb6e80cd6fb2fa455fdc959f337e
anand jain <anandj@uchicago.edu>
2024-04-26 11:18:04 -0700

    rewriting the webcam_capture in c, not done

--- main.c
+++ main.c
@@ -1,38 +1,97 @@
-#include <libavcodec/avcodec.h>
+#include <libavdevice/avdevice.h>
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
-#include <libavutil/imgutils.h>
+#include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
-#include <libavdevice/avdevice.h>
+#include <libswresample/swresample.h>
+#include <libavutil/pixfmt.h>
+#include <libavutil/pixdesc.h>
 
-void list_devices()
+// #include <libswscale/.h>
+
+#include <stdio.h>
+
+int get_stream_idx(const AVFormatContext *formatCtx, enum AVMediaType type)
 {
-    AVFormatContext *pFormatCtx = NULL;
-    AVInputFormat *inputFormat = av_find_input_format("dshow");
-    AVDictionary *options = NULL;
-
-    av_dict_set(&options, "list_devices", "true", 0);
-    int ret = avformat_open_input(&pFormatCtx, "dummy", inputFormat, &options);
-    if (ret < 0)
+    for (int i = 0; i < formatCtx->nb_streams; i++)
     {
-        av_log(NULL, AV_LOG_ERROR, "Cannot open input device\n");
+        if (formatCtx->streams[i]->codecpar->codec_type == type)
+        {
+            return i;
+        }
     }
-    else
-    {
-        av_log(NULL, AV_LOG_INFO, "Device list (above) provided by dshow\n");
-    }
-
-    avformat_close_input(&pFormatCtx);
-    av_dict_free(&options);
+    return -1;
 }
 
-int main(int argc, char **argv)
+int main()
 {
     avdevice_register_all();
-    // list_devices();
-    // AVFormatContext *pFormatCtx = NULL;
+
     AVFormatContext *formatCtx = avformat_alloc_context();
-    AVInputFormat *inputFormat = av_find_input_format("dshow"); // Use "v4l2" for Linux
+    AVInputFormat *inputFormat = av_find_input_format("dshow"); // Use "dshow" on Windows, "v4l2" on Linux
+    avformat_open_input(&formatCtx, "video=Microsoft® LifeCam Studio(TM)", inputFormat, NULL);
+    int stream_idx = get_stream_idx(formatCtx, AVMEDIA_TYPE_VIDEO);
+    printf("Stream index: %d\n", stream_idx);
+    AVCodecContext *codecCtx = avcodec_alloc_context3(NULL);
+    AVCodecParameters *codecParams = formatCtx->streams[stream_idx]->codecpar;
+    printf("stream codecpar res: %dx%d\n", codecParams->width, codecParams->height);
+
+    avcodec_parameters_to_context(codecCtx, codecParams);
+    AVCodec *codec = avcodec_find_decoder(codecCtx->codec_id);
+    avcodec_open2(codecCtx, codec, NULL);
+    // codecCtx->
+    printf("Codec name: %s\n", codec->long_name);
+    printf("Codec ID: %d\n", codecCtx->codec_id);
+
+    struct SwsContext *scaler = sws_getContext(codecCtx->width, codecCtx->height, codecCtx->pix_fmt, codecCtx->width, codecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+    // AVPixelFormat
+    const char *pix_fmt_name = av_get_pix_fmt_name(codecCtx->pix_fmt);
+    printf("Pixel Format: %s\n", pix_fmt_name);
+
+    AVFrame *frame = av_frame_alloc(); // this comes straight from the cam i guess
+    printf("frame fmt %d\n", frame->format);
+
+    // frame->format = AV_PIX_FMT_YUYV422;
+    frame->format = codecCtx->pix_fmt;
+    frame->width = codecCtx->width;
+    frame->height = codecCtx->height;
+    int did_get_buf = av_frame_get_buffer(frame, 32);
+    printf("did get buf: %d\n", did_get_buf);
+    printf("frame fmt after set %d\n", frame->format);
+
+    AVFrame *frame_rgb = av_frame_alloc(); // this is the frame that will be converted to RGB
+    frame_rgb->format = AV_PIX_FMT_RGB24;
+    frame_rgb->width = codecCtx->width;
+    frame_rgb->height = codecCtx->height;
+
+    int rgb_did_get_buf = av_frame_get_buffer(frame_rgb, 32);
+    printf("rgb did get buf: %d\n", rgb_did_get_buf);
     
+    AVPacket *pkt = av_packet_alloc();
+    av_init_packet(pkt);
+    int isempty = frame->data[0] == NULL;
+    printf("Frame is empty: %d\n", isempty);
+    printf("Frame linesize: %d\n", frame->linesize[0]);
+    // printf("Frame linesize 1: %d\n", frame->linesize[1]);
+    // printf("Frame linesize 2: %d\n", frame->linesize[2]);
+    // printf("Frame linesize 2: %d\n", frame->linesize[3]);
+    printf("pkt size %d\n", pkt->size);
+    printf("frame pkt size %d\n", frame->pkt_size);
+    // frame->pkt_size
+    int was_sent = avcodec_send_packet(codecCtx, pkt);
+    printf("Packet sent: %d\n", was_sent);
+    int f_ret = avcodec_receive_frame(codecCtx, frame_rgb);
+    printf("cctx fmt %d\n", codecCtx->pix_fmt);
+    printf("frame fmt %d\n", frame->format);
+    printf("Frame received: %d\n", f_ret);
+    // pripkt->stream_index;
+    printf("pkt stream index: %d\n", pkt->stream_index);
+    printf("rgb linesize[0] = %d\n", frame_rgb->linesize[0]);
+    // scaler = sws_getContext(dec_ctx->width, dec_ctx->height, dec_ctx->pix_fmt, dec_ctx->width, dec_ctx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+    int did_scale = sws_scale(scaler, (const uint8_t *const *)frame->data, frame->linesize, 0, frame->height, frame_rgb->data, frame_rgb->linesize);
+    printf("Did scale: %d\n", did_scale);
+    avcodec_free_context(&codecCtx);
+    sws_freeContext(scaler);
+    avformat_close_input(&formatCtx);
     return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,116 @@
 extern crate ffmpeg_the_third as ffmpeg;
 
+use crate::ffmpeg::codec::packet::Packet;
+use crate::ffmpeg::codec::Context as CodecContext;
 use crate::ffmpeg::format::{input, Pixel};
 use crate::ffmpeg::media::Type;
 use crate::ffmpeg::software::scaling::{context::Context, flag::Flags};
 use crate::ffmpeg::util::frame::video::Video;
-use anyhow;
-use ffmpeg::format::context::Input;
+use crate::ffmpeg::Dictionary;
+
+use crate::ffmpeg::format::context::Output;
+use anyhow::{self};
+use ffmpeg::encoder::{self, find_by_name, Encoder};
 use ffmpeg::format::output;
-use ffmpeg::time;
-use ffmpeg_sys_the_third::{av_find_input_format, avdevice_register_all, avformat_open_input};
-use image::{ImageBuffer, RgbImage};
+use ffmpeg::{codec, Codec};
+use ffmpeg_sys_the_third::{av_find_input_format, avformat_open_input};
 use std::ffi::CString;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::Result as IoResult;
 use std::{env, ptr};
 
 fn main() -> Result<(), anyhow::Error> {
     ffmpeg::init().unwrap();
-    // input()
+
+    let device_name = CString::new("video=Microsoft® LifeCam Studio(TM)").unwrap();
+    let format_name = CString::new("dshow").unwrap();
+    let output_file = "output.mp4";
+    let output_path = std::path::Path::new(output_file);
+    let mut ps = ptr::null_mut();
+    let mut options = ptr::null_mut();
+
+    let mut ictx = unsafe {
+        ffmpeg::device::register_all();
+        let fmt = av_find_input_format(format_name.as_ptr());
+        avformat_open_input(&mut ps, device_name.as_ptr(), fmt, &mut options);
+        ffmpeg::format::context::Input::wrap(ps)
+    };
+
+    let input_stream = ictx.streams().best(Type::Video).unwrap();
+    let video_stream_index = input_stream.index();
+    let decoder = ffmpeg::codec::context::Context::from_parameters(input_stream.parameters())?
+        .decoder()
+        .video()?;
+
+    let mut octx = output(&output_path)?;
+    let c = ffmpeg::codec::id::Id::AV1;
+
+    let mut encoder = encoder::find(c).unwrap();
+    let enc = encoder::find_by_name("libx264").unwrap();
+    let vid = encoder.video().unwrap();
+    codec::Context::encoder()?;
+// Encoder::
+
+    println!("Encoder : {:?}", encoder.name());
+    println!("Encoder : {:?}", encoder.description());
+    unsafe {
+        println!("Encoder : {:?}", *(encoder.as_ptr()));
+
+    }
+    // encoder.capabilities()
+    // unsafe {
+    // }
+    // let mut encoder = Codec::from().unwrap().video()?;
+
+    // encoder.
+    // encoder.set_width(decoder.width());
+    // encoder.set_height(decoder.height());
+    // encoder.set_format(Pixel::YUV420P);
+    // encoder.set_time_base((1, 30));
+    let mut stream = octx.add_stream(encoder).unwrap();
+    // let mut encoder = stream.codec().encoder().video()?;
+    // encoder.set_time_base((1, 30));
+
+    // encoder.open_as(
+    //     Codec::find_encoder(ffmpeg::codec::id::Id::H264).unwrap(),
+    //     &Dictionary::new(),
+    // )?;
+    // stream.set_time_base((1, 30));
+    // octx.write_header()?;
+
+    // let mut scaler = Context::get(
+    //     decoder.format(),
+    //     decoder.width(),
+    //     decoder.height(),
+    //     Pixel::YUV420P,
+    //     decoder.width(),
+    //     decoder.height(),
+    //     Flags::BILINEAR,
+    // )?;
+
+    // for (stream, packet) in ictx.packets() {
+    //     if stream.index() == video_stream_index {
+    //         decoder.send_packet(&packet)?;
+    //         let mut decoded = Video::empty();
+    //         while decoder.receive_frame(&mut decoded).is_ok() {
+    //             let mut converted = Video::empty();
+    //             scaler.run(&decoded, &mut converted)?;
+    //             let mut encoded = Packet::empty();
+    //             encoder.send_frame(&converted)?;
+    //             while encoder.receive_packet(&mut encoded).is_ok() {
+    //                 encoded.set_stream(0);
+    //                 octx.interleaved_write_frame(&encoded)?;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // encoder.send_eof()?;
+    // let mut encoded = Packet::empty();
+    // while encoder.receive_packet(&mut encoded).is_ok() {
+    //     encoded.set_stream(0);
+    //     octx.interleaved_write_frame(&encoded)?;
+    // }
+
+    // octx.write_trailer()?;
 
     Ok(())
 }
