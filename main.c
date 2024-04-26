64f8303032f98c1daec476e696f75b76d4d6fb7c
anand jain <anandj@uchicago.edu>
2024-04-26 11:34:47 -0700

    c code working

--- main.c
+++ main.c
@@ -6,10 +6,90 @@
 #include <libswresample/swresample.h>
 #include <libavutil/pixfmt.h>
 #include <libavutil/pixdesc.h>
-
-// #include <libswscale/.h>
-
+#include <libavutil/opt.h>
+#include <libavutil/imgutils.h>
 #include <stdio.h>
+
+int save_frame_as_png(AVFrame *frame, const char *filename)
+{
+    // Find the PNG encoder
+    AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_PNG);
+    if (!codec)
+    {
+        fprintf(stderr, "Codec not found\n");
+        return -1;
+    }
+
+    AVCodecContext *c = avcodec_alloc_context3(codec);
+    if (!c)
+    {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        return -1;
+    }
+
+    // Set up codec context for the encoder
+    c->bit_rate = 400000;
+    c->width = frame->width;
+    c->height = frame->height;
+    c->pix_fmt = AV_PIX_FMT_RGB24;
+    c->time_base.num = 1;
+    c->time_base.den = 25;
+
+    // Open the codec
+    if (avcodec_open2(c, codec, NULL) < 0)
+    {
+        fprintf(stderr, "Could not open codec\n");
+        avcodec_free_context(&c);
+        return -1;
+    }
+
+    FILE *file = fopen(filename, "wb");
+    if (!file)
+    {
+        fprintf(stderr, "Could not open %s\n", filename);
+        avcodec_free_context(&c);
+        return -1;
+    }
+
+    // Allocate an AVPacket for encoded output
+    AVPacket pkt;
+    av_init_packet(&pkt);
+    pkt.data = NULL; // packet data will be allocated by the encoder
+    pkt.size = 0;
+
+    // Send the frame to the encoder
+    int ret = avcodec_send_frame(c, frame);
+    if (ret < 0)
+    {
+        fprintf(stderr, "Error sending a frame for encoding\n");
+        fclose(file);
+        avcodec_free_context(&c);
+        return -1;
+    }
+
+    while (ret >= 0)
+    {
+        ret = avcodec_receive_packet(c, &pkt);
+        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+            break;
+        else if (ret < 0)
+        {
+            fprintf(stderr, "Error during encoding\n");
+            break;
+        }
+
+        fwrite(pkt.data, 1, pkt.size, file);
+        av_packet_unref(&pkt);
+    }
+
+    fclose(file);
+    avcodec_free_context(&c);
+    return 0;
+}
+
+// ... (rest of your existing main function code)
+
+// Example usage:
 
 int get_stream_idx(const AVFormatContext *formatCtx, enum AVMediaType type)
 {
@@ -36,26 +116,25 @@ int main()
     AVCodecParameters *codecParams = formatCtx->streams[stream_idx]->codecpar;
     printf("stream codecpar res: %dx%d\n", codecParams->width, codecParams->height);
 
-    avcodec_parameters_to_context(codecCtx, codecParams);
+    int did_get_codecCtx = avcodec_parameters_to_context(codecCtx, codecParams);
     AVCodec *codec = avcodec_find_decoder(codecCtx->codec_id);
     avcodec_open2(codecCtx, codec, NULL);
-    // codecCtx->
     printf("Codec name: %s\n", codec->long_name);
     printf("Codec ID: %d\n", codecCtx->codec_id);
 
     struct SwsContext *scaler = sws_getContext(codecCtx->width, codecCtx->height, codecCtx->pix_fmt, codecCtx->width, codecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
-    // AVPixelFormat
+
     const char *pix_fmt_name = av_get_pix_fmt_name(codecCtx->pix_fmt);
     printf("Pixel Format: %s\n", pix_fmt_name);
 
     AVFrame *frame = av_frame_alloc(); // this comes straight from the cam i guess
     printf("frame fmt %d\n", frame->format);
 
-    // frame->format = AV_PIX_FMT_YUYV422;
     frame->format = codecCtx->pix_fmt;
     frame->width = codecCtx->width;
     frame->height = codecCtx->height;
     int did_get_buf = av_frame_get_buffer(frame, 32);
+
     printf("did get buf: %d\n", did_get_buf);
     printf("frame fmt after set %d\n", frame->format);
 
@@ -66,9 +145,11 @@ int main()
 
     int rgb_did_get_buf = av_frame_get_buffer(frame_rgb, 32);
     printf("rgb did get buf: %d\n", rgb_did_get_buf);
-    
+
     AVPacket *pkt = av_packet_alloc();
     av_init_packet(pkt);
+    int did_read_frame = av_read_frame(formatCtx, pkt);
+    printf("Did read frame: %d\n", did_read_frame);
     int isempty = frame->data[0] == NULL;
     printf("Frame is empty: %d\n", isempty);
     printf("Frame linesize: %d\n", frame->linesize[0]);
@@ -79,8 +160,9 @@ int main()
     printf("frame pkt size %d\n", frame->pkt_size);
     // frame->pkt_size
     int was_sent = avcodec_send_packet(codecCtx, pkt);
+
     printf("Packet sent: %d\n", was_sent);
-    int f_ret = avcodec_receive_frame(codecCtx, frame_rgb);
+    int f_ret = avcodec_receive_frame(codecCtx, frame);
     printf("cctx fmt %d\n", codecCtx->pix_fmt);
     printf("frame fmt %d\n", frame->format);
     printf("Frame received: %d\n", f_ret);
@@ -90,6 +172,10 @@ int main()
     // scaler = sws_getContext(dec_ctx->width, dec_ctx->height, dec_ctx->pix_fmt, dec_ctx->width, dec_ctx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
     int did_scale = sws_scale(scaler, (const uint8_t *const *)frame->data, frame->linesize, 0, frame->height, frame_rgb->data, frame_rgb->linesize);
     printf("Did scale: %d\n", did_scale);
+    printf("frame_rgb linesize[0] = %d\n", frame_rgb->linesize[0]);
+    printf("frame_rgb format= %s\n", av_get_pix_fmt_name(frame_rgb->format));
+    save_frame_as_png(frame_rgb, "output.png");
+
     avcodec_free_context(&codecCtx);
     sws_freeContext(scaler);
     avformat_close_input(&formatCtx);
